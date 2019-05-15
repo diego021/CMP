bead70cf49cee4a22297537874acd9610d15ada1
Diego Arias <darias@geocom.com.uy>
2019-05-15 15:28:54 -0500

    Extended ydl_opts in download_mp3; download_archive and outtmpl

--- youtube/downloader.py
+++ youtube/downloader.py
@@ -3,6 +3,8 @@ import youtube_dl
 
 def download_mp3(url):
     ydl_opts = { 'format': 'bestaudio/best',
+                 'download_archive': '/tmp/cmpindex',
+                 'outtmpl': '/home/diego021/Music/%(title)s.%(ext)s',
                  'postprocessors': [{ 'key': 'FFmpegExtractAudio',
                                       'preferredcodec': 'mp3',
                                       'preferredquality': '192' }] }
