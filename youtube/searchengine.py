388a61dec17acb05ff1119fc624df9dd77492157
Diego Arias <darias@geocom.com.uy>
2019-05-16 12:03:16 -0500

    Add searchengine module for youtube

---
+++ youtube/searchengine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import youtube_dl
+
+def search(string: 'text to search', max_duration: 'max duration allowed in seconds'=420) -> list:
+    search_result = youtube_dl.YoutubeDL({}).extract_info('ytsearch10: {}'.format(string), download=False)
+    data = []
+    for entry in search_result['entries']:
+        if len(data) == 5: break
+        if entry['duration'] > max_duration: continue
+        track = { 'title': entry['title'],
+                  'duration': entry['duration'],
+                  'url': entry['webpage_url'] }
+        data.append(track)
+    return data
+
+if __name__ == '__main__':
+    for song in search('blues motel'):
+        print(song)
+
